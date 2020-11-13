9c15bd19614142fce03965b656fffc9fd1c3bf87
Justin Benz <jbenz156@gmail.com>
2020-11-13 18:10:58 -0500

    fixed bugs to the best of my ability

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,9 +6,9 @@ export default function Form (props) {
 
     const changeHandle = (evt) => {
         const { name, value, type, checked } = evt.target;
-        const inputToUse = type === "checkbox" ? name : value;
+        const inputToUse = type === "checkbox" ? checked : value;
         change(name, inputToUse);
-    }
+    };
 
     const submitHandle = (evt) => {
         evt.preventDefault();
@@ -19,7 +19,7 @@ export default function Form (props) {
         <div className="formCover">
             <form className="form">
                 <h1>Name and Size</h1>
-                <input type="text" onChange={changeHandle} value={input.name} placeholder="Name..."></input>
+                <input type="text" name="name" onChange={changeHandle} value={input.name} placeholder="Name..."></input>
                 <select name="size" onChange={changeHandle} value={input.size}>
                     <option value={0}>----</option>
                     <option name="Small" value={1}>Small</option>
@@ -29,39 +29,39 @@ export default function Form (props) {
                 <h1>Sauce</h1>
                 <label>
                     OG Red
-                    <input name="OG Red" onChange={changeHandle} checked={input.sauce} type="checkbox" ></input>
+                    <input name="sauce" onChange={changeHandle} checked={input.sauce} type="checkbox" ></input>
                 </label>
                 <label>
                     Garlic Ranch
-                    <input name="Garlic Ranch" onChange={changeHandle} checked={input.sauce} type="checkbox"></input>
+                    <input name="sauce" onChange={changeHandle} checked={input.sauce} type="checkbox"></input>
                 </label>
                 <label>
                     BBQ Sauce
-                    <input name="BBQ Sauce" onChange={changeHandle} checked={input.sauce} type="checkbox"></input>
+                    <input name="sauce" onChange={changeHandle} checked={input.sauce} type="checkbox"></input>
                 </label>
                 <label>
                     Spinach Alfredo
-                    <input name="Spinach Alfredo" onChange={changeHandle} checked={input.sauce} type="checkbox"></input>
+                    <input name="sauce" onChange={changeHandle} checked={input.sauce} type="checkbox"></input>
                 </label>
                 <h1>Toppings</h1>
                 <label>
                     Pepperoni
-                    <input name="Pepperoni" onChange={changeHandle} checked={input.toppings} type="checkbox"></input>
+                    <input name="toppings" onChange={changeHandle} checked={input.toppings} type="checkbox"></input>
                 </label>
                 <label>
                     Olives
-                    <input name="Olives" onChange={changeHandle} checked={input.toppings} type="checkbox"></input>
+                    <input name="toppings" onChange={changeHandle} checked={input.toppings} type="checkbox"></input>
                 </label>
                 <label>
                     Bacon
-                    <input name="Bacon" checked={input.toppings} onChange={changeHandle} type="checkbox"></input>
+                    <input name="toppings" checked={input.toppings} onChange={changeHandle} type="checkbox"></input>
                 </label>
                 <label>
                     Mushrooms
-                    <input name="Mushrooms" checked={input.toppings} onChange={changeHandle} type="checkbox"></input>
+                    <input name="toppings" checked={input.toppings} onChange={changeHandle} type="checkbox"></input>
                 </label>
                 <h2>Special Instructions</h2>
-                <input type="text" value={input.special} onChange={changeHandle} placeholder="Special Instructions..."></input>
+                <input type="text" name="special" value={input.special} onChange={changeHandle} placeholder="Special Instructions..."></input>
                 <button id="submitBtn">Submit Order</button>
             </form>
         </div>
